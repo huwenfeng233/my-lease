332e31983a43acfd94388828e66573ea8605e056
huwenfeng233 <2339758958@qq.com>
2025-02-02 23:26:07 +0800

    完善支付方式接口

--- web/web-admin/src/main/java/com/bighu/web/admin/controller/apartment/LeaseTermController.java
+++ web/web-admin/src/main/java/com/bighu/web/admin/controller/apartment/LeaseTermController.java
@@ -4,10 +4,13 @@ package com.bighu.web.admin.controller.apartment;
 
 import com.bighu.common.result.Result;
 import com.bighu.model.entity.LeaseTerm;
+import com.bighu.web.admin.service.LeaseTermService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Tag(name = "租期管理")
@@ -15,21 +18,43 @@ import java.util.List;
 @RestController
 public class LeaseTermController {
 
+    @Autowired
+    private LeaseTermService leaseTermService;
     @GetMapping("list")
     @Operation(summary = "查询全部租期列表")
     public Result<List<LeaseTerm>> listLeaseTerm() {
-        return Result.ok();
+        List<LeaseTerm> leaseTerms = new ArrayList<LeaseTerm>;
+        try {
+            leaseTerms = leaseTermService.list();
+        }
+        catch (Exception e)
+        {
+            return Result.fail();
+        }
+
+
+        return Result.ok(leaseTerms);
+
     }
 
     @PostMapping("saveOrUpdate")
     @Operation(summary = "保存或更新租期信息")
     public Result saveOrUpdate(@RequestBody LeaseTerm leaseTerm) {
+        leaseTermService.saveOrUpdate(leaseTerm);
         return Result.ok();
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除租期")
     public Result deleteLeaseTermById(@RequestParam Long id) {
-        return Result.ok();
+
+        LeaseTerm byId = leaseTermService.getById(id);
+        if (byId != null)
+        {
+            leaseTermService.removeById(id);
+            return Result.ok();
+        }
+        return Result.fail();
+
     }
 }
